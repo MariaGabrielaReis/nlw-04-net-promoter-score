2db080c7041dc4364461d9091f9df04aca2edf98
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2021-02-23 22:15:00 -0300

    :tada: :sparkles: Adding day 02 modifications

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,24 +1,10 @@
-import express, { request } from 'express';
-const app = express()
+import 'reflect-metadata';
+import express from 'express';
+import './database';
+import { router } from './routes';
 
-/*
-    GET -> Buscar
-    POST -> Salvar
-    PUT -> Alterar
-    DELETE -> Deletar
-    PATCH -> Alteração específica
-*/
-
-//http://localhost:3333/users
-app.get("/", (request, response) => {
-    return response.json({message: "Hello World"});
-});
-
-// primeiro parâmetro = rota (recurso API)
-//segundo parâmetro = request, responseclear
-app.post("/", (request, response) => {
-    //recebe os dados para salvar
-    return response.json({message:"Dados salvos com sucesso!"});
-});
+const app = express();
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running!"));
