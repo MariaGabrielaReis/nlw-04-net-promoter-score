6e657abaa4ce241a15d2bc1df7ac2d4a45a6bd39
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2021-02-25 21:10:33 -0300

    :tada: :sparkles: Creating table, repository and controller for surveys (day 03)

---
+++ api/src/repositories/SurveysRepository.ts
@@ -0,0 +1,7 @@
+import { Entity, EntityRepository, Repository } from "typeorm";
+import { Survey } from "../models/Survey";
+
+@EntityRepository(Survey)
+class SurveysRepository extends Repository<Survey> {}
+
+export { SurveysRepository };
