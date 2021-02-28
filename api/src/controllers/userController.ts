e218dadfd73f4306cdc0cc3f5f35d7d13bfddcfb
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2021-02-28 15:05:37 -0300

    :tada: :sparkles: Adding validations and refactoring tests

--- api/src/controllers/userController.ts
+++ api/src/controllers/userController.ts
@@ -1,10 +1,23 @@
 import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
 import { UsersRepository } from "../repositories/UsersRepository";
+import * as yup from 'yup';
 
 class UserController {
   async create(request: Request, response: Response) {
     const { name, email } = request.body;
+
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().email().required()
+    });
+
+    try {
+      await schema.validate(request.body, { abortEarly: false })
+
+    } catch (erro) {
+      return response.status(400).json({error: erro}) 
+    }
 
     const usersRepository = getCustomRepository(UsersRepository);
 
